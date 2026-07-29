d48b606ca263ec29b0359e49b0a73ee8ded325cc
Duhydi <23520585@gm.uit.edu.vn>
2026-07-29 15:08:50 +0700

    feat: add dependency injection

--- app/api/v1/endpoints/task.py
+++ app/api/v1/endpoints/task.py
@@ -1,31 +1,46 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 
+from app.dependencies.task import get_task_service
 from app.schemas.task import TaskCreate, TaskUpdate
-from app.services import task_service
 
 router = APIRouter()
 
 
 @router.get("/")
-def get_tasks():
-    return task_service.get_tasks()
+def get_tasks(
+    service=Depends(get_task_service),
+):
+    return service.get_tasks()
 
 
 @router.get("/{task_id}")
-def get_task(task_id: int):
-    return task_service.get_task(task_id)
+def get_task(
+    task_id: int,
+    service=Depends(get_task_service),
+):
+    return service.get_task(task_id)
 
 
 @router.post("/")
-def create_task(task: TaskCreate):
-    return task_service.create_task(task)
+def create_task(
+    task: TaskCreate,
+    service=Depends(get_task_service),
+):
+    return service.create_task(task)
 
 
 @router.put("/{task_id}")
-def update_task(task_id: int, task: TaskUpdate):
-    return task_service.update_task(task_id, task)
+def update_task(
+    task_id: int,
+    task: TaskUpdate,
+    service=Depends(get_task_service),
+):
+    return service.update_task(task_id, task)
 
 
 @router.delete("/{task_id}")
-def delete_task(task_id: int):
-    return task_service.delete_task(task_id)
+def delete_task(
+    task_id: int,
+    service=Depends(get_task_service),
+):
+    return service.delete_task(task_id)
