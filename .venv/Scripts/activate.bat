42d5e7bb3d6134980a099e97a890ca66a586e2da
Duhydi <23520585@gm.uit.edu.vn>
2026-07-31 17:09:51 +0700

    chore: setup local development environment

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\Intern Py\TaskHub\taskhub-api\.venv
+set VIRTUAL_ENV=D:\Intern Py\TaskHub\taskhup-api\taskhub-api\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
